5e8ee9b8ed021de73842c5905eb9954a77997d8a
pranavcoding2010 <110837756+pranavcoding2010@users.noreply.github.com>
2022-09-23 20:14:03 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,48 @@
+Name = (input('Enter your name:'))
+print("Hello",Name,"Today lets check your general knowledge")
+print("a.What is the capital of India ?")
+print("1.New Delhi")
+print("2.Rome")
+print("3.Tokyo")
+print("Please enter 1,2,3")
+Answer1 = int(input('Enter your option'))
+if Answer1 == 1 :
+    print("Correct answer")
+else :
+    print("Incorrect answer")
+    print("The correct answer is New Delhi")
+print("b.What is the national animal of India ?")
+print("1.Rufous hornero")
+print("2.Fennec fox")
+print("3.Tiger")
+print("Please enter 1,2,3")
+Answer2 = int (input ('Enter your option'))
+if Answer2 == 3 :
+    print("Correct answer")
+else :
+    print("Incorrect answer")
+    print("The correct answer is Tiger")
+
+print("b.What is the national bird of India ?")
+print("1.Sparrow")
+print("2.Peacock")
+print("3.Eagle")
+print("Please enter 1,2,3")
+Answer3 = int(input('Enter your option'))
+if Answer3 == 2 :
+    print("Correct answer")
+else :
+    print("Incorrect answer")
+    print("The correct answer is Peacock")
+
+print("c.What is the national flower of India ?")
+print("1.Lotus")
+print("2.Golden Wattle")
+print("3.Sunflower")
+print("Please enter 1,2,3")
+Answer4 = int(input('Enter your option'))
+if Answer4 == 1 :
+    print("Correct answer")
+else : 
+    print("Incorrect answer")
+    print("The correct answer is Lotus")
